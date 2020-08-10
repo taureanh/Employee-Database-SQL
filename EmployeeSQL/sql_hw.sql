5cf5b716017cecd9f9cb751edb4787bdf3d3f2e6
taureanh <taurean.hutchinson@gmail.com>
2020-08-10 00:16:23 -0400

    update

--- EmployeeSQL/sql_hw.sql
+++ EmployeeSQL/sql_hw.sql
@@ -66,37 +66,37 @@ REFERENCES "employees" ("emp_no");
 
 select * from employees
 
---1
+--1.List the following details of each employee: employee number, last name, first name, sex, and salary.
 select employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 from employees
 inner join salaries on
 employees.emp_no = salaries.emp_no;
 
 
---2
+--2. List first name, last name, and hire date for employees who were hired in 1986.
 select emp_no, last_name, first_name, hire_date
 from employees
 where hire_date >= '1986-01-01 00:00:00'  and hire_date < '1987-01-01 00:00:00';
 
---3
+--3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 select departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.first_name, employees.last_name
 from departments
 inner join dept_manager on departments.dept_no = dept_manager.dept_no
 inner join employees on dept_manager.emp_no = employees.emp_no;
 
---4
+--4. List the department of each employee with the following information: employee number, last name, first name, and department name.
 select employees.emp_no, departments.dept_name,  employees.last_name, employees.first_name
 from departments
 inner join dept_emp on departments.dept_no = dept_emp.dept_no
 inner join employees on employees.emp_no = dept_emp.emp_no
 order by emp_no asc;
 
---5
+--5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 select first_name, last_name, sex
 from employees
 where first_name = 'Hercules' and last_name like 'B%';
 
---6
+--6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 select employees.emp_no, departments.dept_name,  employees.last_name, employees.first_name
 from departments
 inner join dept_emp on departments.dept_no = dept_emp.dept_no
@@ -104,7 +104,7 @@ inner join employees on employees.emp_no = dept_emp.emp_no
 where dept_name ='Sales'
 order by emp_no asc;
 
---7
+--7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name
 select employees.emp_no, departments.dept_name,  employees.last_name, employees.first_name
 from departments
 inner join dept_emp on departments.dept_no = dept_emp.dept_no
@@ -112,7 +112,7 @@ inner join employees on employees.emp_no = dept_emp.emp_no
 where dept_name ='Sales' or dept_name= 'Development'
 order by emp_no asc;
 
---8
+--8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 select last_name,count(last_name) as Frequency 
 from employees 
 group by last_name
